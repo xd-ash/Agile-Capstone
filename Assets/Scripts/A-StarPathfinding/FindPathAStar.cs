ba8a509bf144e73afb02543840c3300efc1ac6b3
Alease21 <alease21@outlook.com>
2025-10-21 14:45:49 -0500

    10-21 Class
    not much done

--- Assets/Scripts/A-StarPathfinding/FindPathAStar.cs
+++ Assets/Scripts/A-StarPathfinding/FindPathAStar.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static UnityEngine.RuleTile.TilingRuleOutput;
 
 namespace AStarPathfinding
 {
