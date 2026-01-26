5badf9a83aba1d04aa547d175470337322c18d0d
xd-ash <asherkier@gmail.com>
2026-01-26 13:47:42 -0600

    Small Deck Changes

--- Assets/Scripts/Managers/Combat/CombatBalance.cs
+++ Assets/Scripts/Managers/Combat/CombatBalance.cs
@@ -4,17 +4,17 @@ public class CombatBalance: MonoBehaviour
 {
     //Might be useless if we set hit chances in the cards themselves
     [Header("Base Hit Chance")]
-    [Range(0, 100)] public int baseHitChance = 80;
-    [Range(0, 100)] public int minHitChance = 10;
-    [Range(0, 100)] public int maxHitChance = 95;
+    [Range(0, 100)] public int baseHitChance;
+    [Range(0, 100)] public int minHitChance;
+    [Range(0, 100)] public int maxHitChance;
 
     [Header("Distance Penalties")]
     //public int noPenaltyRange = 3;
-    public int hitPenaltyPerTile = 5;
+    public int hitPenaltyPerTile;
     
     [Header("Accuracy Modifiers")] 
     //Multiplies hit chance: 1 = normal, 0.8 = harder, 1.2 = easier
-    public float accuracyMultiplier = 1;
+    public float accuracyMultiplier;
     //Flat bonus added to all hit chances after multiplier
     public int accuracyFlatBonus;
 
