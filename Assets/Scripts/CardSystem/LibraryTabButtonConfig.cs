b4c434437d317ba613ccc54cf7a0f44fe754218a
Alease21 <alease21@outlook.com>
2026-04-30 21:36:36 -0500

    Small systems
    -added a rewards hover on the nodemap nodes
    -added a card library viewer from main menu with tabs for sorting card by category
    -adjusted nodemap balance a bit to avoid shops and camps being too close

--- Assets/Scripts/CardSystem/LibraryTabButtonConfig.cs
+++ Assets/Scripts/CardSystem/LibraryTabButtonConfig.cs
@@ -8,15 +8,20 @@ public class LibraryTabButtonConfig : MonoBehaviour
     public Vector2 inactiveSize;
     public bool toggleOnEnable;
 
+    [SerializeField, Space(10)] private GameObject _scrollView;
+    [SerializeField, Range(0f, 1f)] private float _scrollViewAlphaMultiplier;
+
     private Button _button;
     private TextMeshProUGUI _tabText;
     RectTransform _transform;
+    private Image _scrollViewImage;
 
     private void Awake()
     {
         _transform = GetComponent<RectTransform>();
         _button = GetComponent<Button>();
         _tabText = GetComponentInChildren<TextMeshProUGUI>(true);
+        _scrollViewImage = _scrollView?.GetComponent<Image>();
     }
 
     private void OnEnable()
@@ -29,5 +34,12 @@ public class LibraryTabButtonConfig : MonoBehaviour
     {
         _transform.sizeDelta = isActive ? activeSize : inactiveSize;
         _tabText.gameObject.SetActive(isActive);
+
+        if (!isActive) return;
+
+        if (_scrollViewImage == null)
+            _scrollViewImage = _scrollView?.GetComponent<Image>();
+        if (_scrollViewImage != null)
+            _scrollViewImage.color =  new Color(_button.image.color.r, _button.image.color.g, _button.image.color.b, _scrollViewAlphaMultiplier);
     }
 }
