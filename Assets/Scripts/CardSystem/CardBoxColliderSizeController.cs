47c15709ab1cf4938266f04099a626b5ed9383aa
Alease21 <alease21@outlook.com>
2026-05-01 03:23:54 -0500

    card BC tweaks done
    -added small script that controls the size of a card's box collider
    -script will lerp the BC size & offset x values based on the ratio of current cards to max hand size in order to make card selection easier
    -adjusted the card prefab to use only 1 BC that is controller by new script. Previously was 2 box colliders & a composite collider because I was being dumb

--- Assets/Scripts/CardSystem/CardBoxColliderSizeController.cs
+++ Assets/Scripts/CardSystem/CardBoxColliderSizeController.cs
@@ -1,7 +1,7 @@
 using CardSystem;
 using UnityEngine;
 
-public class CardBoxColliderSizeContorller : MonoBehaviour
+public class CardBoxColliderSizeController : MonoBehaviour
 {
     [Header("Card Hand BC Lerp")]
     // vector2s representing min & max offset x and min & max size x **Vector2(min x, max x)
@@ -9,13 +9,13 @@ public class CardBoxColliderSizeContorller : MonoBehaviour
     [SerializeField] private Vector2 _sizeMinMaxX;
 
     [Header("Combat Entensions")]
-    [SerializeField] private Vector2 _sizeMinMaxY;
+    [SerializeField] private Vector2 _offsetAndSizeMaxY;
 
-    private BoxCollider2D _mainBC;
+    private BoxCollider2D _bc;
 
     private void Awake()
     {
-        _mainBC = GetComponents<BoxCollider2D>()?[0];
+        _bc = GetComponent<BoxCollider2D>();
 
         if (DeckAndHandManager.Instance == null) return;
         DeckAndHandManager.OnUpdateCardColliders += LerpBCSizeAndOffset;
@@ -31,25 +31,24 @@ public class CardBoxColliderSizeContorller : MonoBehaviour
     public void LerpBCSizeAndOffset(bool isHovered)
     {
         var dhm = DeckAndHandManager.Instance;
-        if (dhm == null || _mainBC == null) return;
+        if (dhm == null || _bc == null) return;
         var cardHandRatio = (float)dhm.CardsInHand.Count / (float)dhm.GetMaxHandSize;
         var thisSiblingIndex = transform.GetSiblingIndex();
 
         bool isLastCardChild = thisSiblingIndex >= dhm.CardsInHand.Count - 1;
 
-        var maxOffset = new Vector2(_offsetMinMaxX.y, _mainBC.offset.y);
-        var minOffset = new Vector2(_offsetMinMaxX.x, _mainBC.offset.y);
-        var maxSize = new Vector2(_sizeMinMaxX.y, _mainBC.size.y);
-        var minSize = new Vector2(_sizeMinMaxX.x, _mainBC.size.y);
+        var maxOffset = new Vector2(_offsetMinMaxX.y, _bc.offset.y);
+        var minOffset = new Vector2(_offsetMinMaxX.x, _bc.offset.y);
+        var maxSize = new Vector2(_sizeMinMaxX.y, _bc.size.y);
+        var minSize = new Vector2(_sizeMinMaxX.x, _bc.size.y);
 
-        _mainBC.offset = isLastCardChild || isHovered ? maxOffset : Vector2.Lerp(maxOffset, minOffset, cardHandRatio);
-        _mainBC.size = isLastCardChild || isHovered ? maxSize : Vector2.Lerp(maxSize, minSize, cardHandRatio);
+        _bc.offset = isLastCardChild || isHovered ? maxOffset : Vector2.Lerp(maxOffset, minOffset, cardHandRatio);
+        _bc.size = isLastCardChild || isHovered ? maxSize : Vector2.Lerp(maxSize, minSize, cardHandRatio);
     }
-    public void ExtendBCForCombat(bool isExtended)
+    public void ExtendBCForCombat()
     {
-        var maxSize = new Vector2(_mainBC.size.x, _sizeMinMaxY.y);
-        var minSize = new Vector2(_mainBC.size.x, _sizeMinMaxY.x);
-
-        _mainBC.size = isExtended ? maxSize : minSize;
+        if (_bc == null) return;
+        _bc.offset = new Vector2(_bc.offset.x, _offsetAndSizeMaxY.x);
+        _bc.size = new Vector2(_bc.size.x, _offsetAndSizeMaxY.y);
     }
 }
--- Assets/Scripts/Rewards/CardPrefabSetterUpper.cs
+++ Assets/Scripts/Rewards/CardPrefabSetterUpper.cs
@@ -94,11 +94,11 @@ public static class CardPrefabSetterUpper
     }
     private static void ToggleExtendedBoxCollider(Transform cardTrans, bool enable)
     {
-        var boxColliders = cardTrans.GetComponents<BoxCollider2D>();
+        var bcController = cardTrans.GetComponent<CardBoxColliderSizeController>();
 
-        if (boxColliders.Length <= 1) return;
+        if (bcController == null) return;
 
-        boxColliders[1].enabled = enable;
+        bcController.ExtendBCForCombat();
     }
     private static bool FillTextFields(Card card)
     {
